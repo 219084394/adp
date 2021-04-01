3e3ef5b4b80d7b4a012ae167999d27188cd1d4ed
Anicka Schouw <anickaschouw@gmail.com>
2021-04-01 22:01:24 +0200

    Renamed class and added attributes and methods.

---
+++ src/main/java/za/ac/cput/adp/ASchouw.java
@@ -0,0 +1,57 @@
+package za.ac.cput.adp;
+
+public class ASchouw {
+
+    private String equal,identify,fail, timeout,disable;
+
+    public String getEqual() {
+        return equal;
+    }
+
+    public void setEqual(String equal) {
+        this.equal = equal;
+    }
+
+    public String getIdentify() {
+        return identify;
+    }
+
+    public void setIdentify(String identify) {
+        this.identify = identify;
+    }
+
+    public String getFail() {
+        return fail;
+    }
+
+    public void setFail(String fail) {
+        this.fail = fail;
+    }
+
+    public String getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(String timeout) {
+        this.timeout = timeout;
+    }
+
+    public String getDisable() {
+        return disable;
+    }
+
+    public void setDisable(String disable) {
+        this.disable = disable;
+    }
+
+    @Override
+    public String toString() {
+        return "ASchouw{" +
+                "equal='" + equal + '\'' +
+                ", identify='" + identify + '\'' +
+                ", fail='" + fail + '\'' +
+                ", timeout='" + timeout + '\'' +
+                ", disable='" + disable + '\'' +
+                '}';
+    }
+}
