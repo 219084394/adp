e267b6276058f9678788af1532a3cd8ab863787c
Anicka Schouw <anickaschouw@gmail.com>
2021-04-01 16:52:14 +0200

    Anicka Schouw added and merged her work.

--- src/main/java/za/ac/cput/adp/QuestionFour.java
+++ src/main/java/za/ac/cput/adp/QuestionFour.java
@@ -1,7 +1,63 @@
 package za.ac.cput.adp;
 
+/*
+ *@Author Anicka Schouw 217284183
+ * Assignment 1 2021
+ */
+
+
 public class QuestionFour {
-    public static void main(String[] args) {
-        System.out.println("placeholder");
+//Anicka Schouw 217284183
+     private String equal,identify,fail, timeout,disable;
+
+    public String getEqual() {
+        return equal;
+    }
+
+    public void setEqual(String equal) {
+        this.equal = equal;
+    }
+
+    public String getIdentify() {
+        return identify;
+    }
+
+    public void setIdentify(String identify) {
+        this.identify = identify;
+    }
+
+    public String getFail() {
+        return fail;
+    }
+
+    public void setFail(String fail) {
+        this.fail = fail;
+    }
+
+    public String getTimeout() {
+        return timeout;
+    }
+
+    public void setTimeout(String timeout) {
+        this.timeout = timeout;
+    }
+
+    public String getDisable() {
+        return disable;
+    }
+
+    public void setDisable(String disable) {
+        this.disable = disable;
+    }
+
+    @Override
+    public String toString() {
+        return "QuestionFour{" +
+                "equal='" + equal + '\'' +
+                ", identify='" + identify + '\'' +
+                ", fail='" + fail + '\'' +
+                ", timeout='" + timeout + '\'' +
+                ", disable='" + disable + '\'' +
+                '}';
     }
 }
